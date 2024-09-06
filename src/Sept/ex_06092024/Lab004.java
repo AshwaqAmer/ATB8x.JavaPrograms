7adec0c8da25f96cc8e9ea391db76f1261b47751
AshwaqAmer <mdashwaqamer@gmail.com>
2024-09-06 17:47:13 +0530

    Add files

--- src/Sept/ex_06092024/Lab004.java
+++ src/Sept/ex_06092024/Lab004.java
@@ -1,10 +1,14 @@
 package Sept.ex_06092024;
 
 public class Lab004 {
-    public static void main (String [] args)
+    public static void main (String [] ashwaq)
     {
         System.out.println("HELLO WORLD");
-        System.out.println();
+        System.out.println(23+23);
+        System.out.println(23-23);
+        System.out.println(23*23);
+        System.out.println(23/23);
+
     }
 
 }
